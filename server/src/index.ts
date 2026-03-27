cdc7eef815ad3312d364bb4fbe9a1aca8534e367
Zaid <zaidshaikh0024@gmail.com>
2026-03-27 16:26:08 +0530

    feat: implement mongodb database connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,8 @@ import cors from "cors";
 import { Env } from "./config/env.config";
 import { asyncHandler } from "./middlewares/asyncHandler.middleware";
 import { HTTPSTATUS } from "./config/http.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import connectDatabase from "./config/database.config";
 
 const app = express();
 
@@ -25,6 +27,9 @@ app.get("/health", asyncHandler(async(req:Request, res: Response) => {
     })
 }))
 
-app.listen(Env.PORT, () => {
+app.use(errorHandler)
+
+app.listen(Env.PORT, async () => {
+    await connectDatabase()
     console.log(`Server running on port ${Env.PORT} in ${Env.NODE_ENV} mode`);
 })
